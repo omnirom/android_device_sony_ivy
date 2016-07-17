ef3441c68f511c35c8d046bcac9426bce9b0bc6d
Humberto Borba <humberos@gmail.com>
2016-07-17 01:04:19 -0300

    ivy: Move omni custom configuration to common
    Depends on:
    https://gerrit.omnirom.org/#/c/18573
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I27ff78eba154a714b5e8f27198a221b0a01141ae

--- omni_ivy.mk
+++ omni_ivy.mk
@@ -23,9 +23,6 @@ $(call inherit-product, device/sony/ivy/aosp_e6553.mk)
 $(call inherit-product, device/sony/common/radio.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit Omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Override Product Name for OmniROM
 PRODUCT_NAME := omni_ivy
 PRODUCT_MODEL := Xperia Z3+
